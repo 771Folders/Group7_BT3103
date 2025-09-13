dac950ad393630d35d29fb28c218bcd4f3576365
771 Folders <xeanstephen05@gmail.com>
2025-09-13 22:26:39 +0800

    Created Patient Model, Repository, and Controller

---
+++ EventDriven.Project.Businesslogic/Controller/PatientController.cs
@@ -0,0 +1,15 @@
+﻿using EventDriven.Project.Model;
+using EventDriven.Project.Businesslogic.Repository;
+
+namespace EventDriven.Project.Businesslogic.Controller
+{
+    public class PatientController
+    {
+        private PatientRepository patientRepo;
+
+        public PatientController()
+        {
+            patientRepo = new PatientRepository();
+        }
+    }
+}
---
+++ EventDriven.Project.Model/PatientModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EventDriven.Project.Model
+{
+    public class PatientModel
+    {
+        public int PatientID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public long Phone { get; set; }
+        public string Email { get; set; }
+        public string EmergencyContact { get; set; }
+        public string DateRegistered { get; set; }
+    }
+}
