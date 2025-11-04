a6e7cde174b685f93e0e5013868bb3c3d264a0ab
Joyce Ann Gatdula <joyceanngatdula30@gmail.com>
2025-11-04 20:11:32 +0800

    Added Billing and Discharge

--- TrinityCareMedica.UI/UserControls/Billing3.Designer.cs
+++ TrinityCareMedica.UI/UserControls/Billing3.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace TrinityCareMedica.UI.UserControls
 {
-    partial class Billing
+    partial class Billing3
     {
-        /// <summary> 
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,22 +20,23 @@
             base.Dispose(disposing);
         }
 
-        #region Component Designer generated code
+        #region Windows Form Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             panel1 = new Panel();
             labelTitle = new Label();
-            button1 = new Button();
             dataGridView1 = new DataGridView();
             ServiceDescription = new DataGridViewTextBoxColumn();
             Quantity = new DataGridViewTextBoxColumn();
             UnitPrice = new DataGridViewTextBoxColumn();
             Total = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button2 = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -44,10 +45,10 @@
             // 
             panel1.BackColor = Color.Teal;
             panel1.Controls.Add(labelTitle);
-            panel1.Location = new Point(60, 50);
+            panel1.Location = new Point(51, 8);
             panel1.Name = "panel1";
             panel1.Size = new Size(1400, 60);
-            panel1.TabIndex = 98;
+            panel1.TabIndex = 106;
             // 
             // labelTitle
             // 
@@ -60,26 +61,16 @@
             labelTitle.TabIndex = 0;
             labelTitle.Text = "Billing";
             // 
-            // button1
-            // 
-            button1.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(1296, 933);
-            button1.Name = "button1";
-            button1.Size = new Size(153, 53);
-            button1.TabIndex = 101;
-            button1.Text = "Payment";
-            button1.UseVisualStyleBackColor = true;
-            // 
             // dataGridView1
             // 
             dataGridView1.BackgroundColor = Color.White;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { ServiceDescription, Quantity, UnitPrice, Total });
-            dataGridView1.Location = new Point(60, 154);
+            dataGridView1.Location = new Point(51, 111);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(1400, 720);
-            dataGridView1.TabIndex = 106;
+            dataGridView1.Size = new Size(1400, 747);
+            dataGridView1.TabIndex = 108;
             // 
             // ServiceDescription
             // 
@@ -109,18 +100,38 @@
             Total.Name = "Total";
             Total.Width = 125;
             // 
-            // Billing
+            // button1
             // 
-            AutoScaleDimensions = new SizeF(14F, 29F);
+            button1.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(1287, 891);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 53);
+            button1.TabIndex = 107;
+            button1.Text = "Exit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(1114, 891);
+            button2.Name = "button2";
+            button2.Size = new Size(153, 53);
+            button2.TabIndex = 109;
+            button2.Text = "Print";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Billing3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightSeaGreen;
+            ClientSize = new Size(1502, 953);
+            Controls.Add(button2);
+            Controls.Add(panel1);
             Controls.Add(dataGridView1);
             Controls.Add(button1);
-            Controls.Add(panel1);
-            Font = new Font("Microsoft JhengHei UI", 14F);
-            Margin = new Padding(5, 4, 5, 4);
-            Name = "Billing";
-            Size = new Size(1520, 1000);
+            Name = "Billing3";
+            Text = "Billing3";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -131,11 +142,12 @@
 
         private Panel panel1;
         private Label labelTitle;
-        private Button button1;
         private DataGridView dataGridView1;
         private DataGridViewTextBoxColumn ServiceDescription;
         private DataGridViewTextBoxColumn Quantity;
         private DataGridViewTextBoxColumn UnitPrice;
         private DataGridViewTextBoxColumn Total;
+        private Button button1;
+        private Button button2;
     }
 }
